cfa45ec99049806daad8e9148a8dbc495fff6bb4
Marcelo Ortiz M <70643379+mortizm1988@users.noreply.github.com>
2024-04-25 11:18:59 +0200

    move treesitter conf to a different file

--- lua/plugins/quarto.lua
+++ lua/plugins/quarto.lua
@@ -36,90 +36,6 @@ return {
   },
 
   {
-    'nvim-treesitter/nvim-treesitter',
-    dependencies = {
-      { 'nvim-treesitter/nvim-treesitter-textobjects' },
-    },
-    run = ':TSUpdate',
-    config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('nvim-treesitter.configs').setup {
-        auto_install = true,
-        ensure_installed = {
-          'r',
-          'python',
-          'markdown',
-          'markdown_inline',
-          'julia',
-          'bash',
-          'yaml',
-          'lua',
-          'vim',
-          'query',
-          'vimdoc',
-          'latex',
-          'html',
-          'css',
-          'dot',
-          'javascript',
-          'mermaid',
-          'norg',
-          'typescript',
-        },
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = false,
-        },
-        indent = {
-          enable = true,
-        },
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = 'gnn',
-            node_incremental = 'grn',
-            scope_incremental = 'grc',
-            node_decremental = 'grm',
-          },
-        },
-        textobjects = {
-          select = {
-            enable = true,
-            lookahead = true,
-            keymaps = {
-              -- You can use the capture groups defined in textobjects.scm
-              ['af'] = '@function.outer',
-              ['if'] = '@function.inner',
-              ['ac'] = '@class.outer',
-              ['ic'] = '@class.inner',
-            },
-          },
-          move = {
-            enable = true,
-            set_jumps = true, -- whether to set jumps in the jumplist
-            goto_next_start = {
-              [']m'] = '@function.outer',
-              [']]'] = '@class.inner',
-            },
-            goto_next_end = {
-              [']M'] = '@function.outer',
-              [']['] = '@class.outer',
-            },
-            goto_previous_start = {
-              ['[m'] = '@function.outer',
-              ['[['] = '@class.inner',
-            },
-            goto_previous_end = {
-              ['[M'] = '@function.outer',
-              ['[]'] = '@class.outer',
-            },
-          },
-        },
-      }
-    end,
-  },
-
-  {
     'neovim/nvim-lspconfig',
     dependencies = {
       { 'williamboman/mason.nvim' },
@@ -691,31 +607,17 @@ return {
   },
 
   -- preview equations
-  {
-    '//jbyuki/nabla.nvim',
-    keys = {
-      { '<leader>qm', ':lua require"nabla".toggle_virt()<cr>', desc = 'toggle [m]ath equations' },
-    },
-  },
-
-  {
-    'benlubas/molten-nvim',
-    enabled = false,
-    build = ':UpdateRemotePlugins',
-    init = function()
-      vim.g.molten_image_provider = 'image.nvim'
-      vim.g.molten_output_win_max_height = 20
-      vim.g.molten_auto_open_output = false
-    end,
-    keys = {
-      { '<leader>mi', ':MoltenInit<cr>', desc = '[m]olten [i]nit' },
-      {
-        '<leader>mv',
-        ':<C-u>MoltenEvaluateVisual<cr>',
-        mode = 'v',
-        desc = 'molten eval visual',
-      },
-      { '<leader>mr', ':MoltenReevaluateCell<cr>', desc = 'molten re-eval cell' },
-    },
-  },
+  -- {
+  --   '//jbyuki/nabla.nvim',
+  --   keys = {
+  --     { '<leader>qm', ':lua require"nabla".toggle_virt()<cr>', desc = 'toggle [m]ath equations' },
+  --   },
+  -- },
+  --
+  -- {
+  --   'benlubas/molten-nvim',
+  --   enabled = false,
+  --   build = ':UpdateRemotePlugins',
+  --   init = function()
+  --     vim.g.molten_image_provid- },
 }
